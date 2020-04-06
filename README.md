c899265281d16805256b6b2a13981595d64264cd
Kevin Wylder <wylderkevin@gmail.com>
2020-04-06 11:47:14 -0700

    Build dist directory based off envirionment

---
+++ README.md
@@ -0,0 +1,13 @@
+# skateboard vision frontend
+
+This is the web application code to contribute to the skateboard vision project. 
+
+Originally this was a directory and build step of [the skateboard vision project](https://github.com/kevinwylder/sbvision), but now it has a separate repo.
+
+### production environment
+
+`yarn build` runs a production webpack build, and points the api at https://sbvision.kwylder.com
+
+### dev environment
+
+`yarn dev` starts webpack in watch mode, and builds the API package pointed to `localhost:1080`.
--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,37 @@ export const API_URL = "${url}";
   `);
 }
 
+function writeIndexHTML(mode) {
+  fs.copyFileSync(`./node_modules/react/umd/react.${mode}.js`, `./dist/react.${mode}.js`);
+  fs.copyFileSync(`./node_modules/react-dom/umd/react-dom.${mode}.js`, `./dist/react-dom.${mode}.js`);
+  fs.writeFileSync("./dist/index.html", `
+<head>
+  <title> Skateboard Vision Project </title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+  <div id="root"/>
+  <script type="text/javascript" src="./react.${mode}.js"></script>
+  <script type="text/javascript" src="./react-dom.${mode}.js"></script>
+  <script type="text/javascript" src="./main.js"></script>
+
+</body>
+  `)
+}
+
 module.exports = env => {
 
   let isDev = env && env.dev;
   if (isDev) {
     writeApiURL(`http://localhost:1080`);
+    writeIndexHTML("development");
   } else {
     writeApiURL(`https://sbvision.kwylder.com`);
+    writeIndexHTML("production.min");
   }
+
 
   return {
     mode: (isDev) ? "development" : "production",
